baeca34ac7f4413961407e51a90e24876d527ffc
Hideyuki Kagami <hide212131@gmail.com>
2024-03-20 22:08:07 +0900

    refactor sub generator

--- generators/spring-ai-llamacpp/generator.mjs
+++ generators/spring-ai-llamacpp/generator.mjs
@@ -1,8 +1,26 @@
 import BaseApplicationGenerator from 'generator-jhipster/generators/base-application';
-import { javaMainPackageTemplatesBlock, moveToJavaPackageSrcDir } from 'generator-jhipster/generators/java/support';
+import { javaMainPackageTemplatesBlock } from 'generator-jhipster/generators/java/support';
 import command from './command.mjs';
-import { getPomElements } from './utils-maven.mjs';
+import { getPomVersionProperties } from 'generator-jhipster/generators/server/support';
 export default class extends BaseApplicationGenerator {
+  async beforeQueue() {
+    await this.dependsOnJHipster('jhipster-llm:spring-ai');
+  }
+
+  get [BaseApplicationGenerator.PREPARING]() {
+    return this.asPreparingTaskGroup({
+      async defaultTask({ application }) {
+        const pomFile = this.readTemplate(this.templatePath('../resources/pom.xml'));
+        // TODO use application.javaDependencies
+        const versions = getPomVersionProperties(pomFile);
+        application.javaDependencies = this.prepareDependencies({
+          ...application.javaDependencies,
+          ...versions,
+        });
+      },
+    });
+  }
+
   get [BaseApplicationGenerator.INITIALIZING]() {
     return this.asInitializingTaskGroup({
       async initializingTemplateTask() {
@@ -20,7 +38,7 @@ export default class extends BaseApplicationGenerator {
             type: 'list',
             name: 'llmModelName',
             message: 'Would you like to use a LLM model (https://ollama.com/library)?',
-            choices: retrieveModels(),
+            choices: retrieveModels,
             default: 'mistral',
           },
           this.blueprintStorage,
@@ -29,27 +47,12 @@ export default class extends BaseApplicationGenerator {
     });
   }
 
-  get [BaseApplicationGenerator.CONFIGURING]() {
-    return this.asConfiguringTaskGroup({
-      async configuringTemplateTask() {},
-    });
-  }
-
   get [BaseApplicationGenerator.LOADING]() {
     return this.asLoadingTaskGroup({
       prepareForTemplates({ application }) {
         const { llmModelName } = this.blueprintConfig;
-        application.llmModelUrl = retrieveModels().find(model => model.value === llmModelName).url;
+        application.llmModelUrl = retrieveModels.find(model => model.value === llmModelName).url;
         application.llmModelName = application.llmModelUrl.split('/').pop();
-      },
-    });
-  }
-
-  get [BaseApplicationGenerator.PREPARING]() {
-    return this.asPreparingTaskGroup({
-      async defaultTask({ application }) {
-        const pomFile = this.readTemplate(this.templatePath('../resources/pom.xml'));
-        application.llmPomAdditions = getPomElements(pomFile);
       },
     });
   }
@@ -63,31 +66,11 @@ export default class extends BaseApplicationGenerator {
               {
                 ...javaMainPackageTemplatesBlock(),
                 templates: [
-                  'config/ChatCompletionRequestMessageDeserializer.java',
-                  'config/JacksonConfig.java',
                   'config/LlamaCppAutoConfiguration.java',
                   'config/LlamaCppProperties.java',
-                  'web/rest/chat/ModelsApiController.java',
                   'service/llm/LlamaPrompt.java',
                   'service/llm/LlamaCppChatClient.java',
                 ],
-              },
-              {
-                condition: generator => generator.clientFrameworkAny,
-                path: `src/main/java/_package_/`,
-                renameTo: moveToJavaPackageSrcDir,
-                templates: [
-                  data => `web/rest/chat/ChatApiController_${data.imperativeOrReactive}.java`,
-                  data => `web/rest/chat/ChatApi_${data.imperativeOrReactive}.java`,
-                ],
-              },
-              {
-                path: 'src/main/resources',
-                templates: ['swagger/api.yml'],
-              },
-              {
-                path: './',
-                templates: ['models/download-model.mjs', 'models/README.md', 'build-chat.mjs'],
               },
             ],
           },
@@ -98,46 +81,13 @@ export default class extends BaseApplicationGenerator {
     });
   }
 
-  get [BaseApplicationGenerator.WRITING_ENTITIES]() {
-    return this.asWritingEntitiesTaskGroup({
-      async writingEntitiesTemplateTask() {},
-    });
-  }
-
   get [BaseApplicationGenerator.POST_WRITING]() {
     return this.asPostWritingTaskGroup({
-      async customizePackageJson() {
-        this.packageJson.merge({
-          scripts: {
-            'llm:download-model': 'node models/download-model.mjs',
-            'webapp:build:dev': 'webpack --config webpack/webpack.dev.js --env stats=minimal && npm run webapp:build:dev:chat --',
-            'webapp:build:dev:chat': 'node build-chat.mjs',
-          },
-        });
-      },
-      async customizeMainApplication({ application: { mainJavaPackageDir, mainClass } }) {
-        this.editFile(`${mainJavaPackageDir}${mainClass}.java`, { ignoreNonExisting: true }, contents =>
-          contents
-            .replace(
-              "Application '{}' is running! Access URLs:",
-              `Application '{}' is running! Access URLs:
-                \\tChat: \\t\\t{}://localhost:{}{}chat-ui/index.html`,
-            )
-            .replace(
-              'applicationName,',
-              `applicationName,
-            protocol,
-            serverPort,
-            contextPath,`,
-            ),
-        );
-      },
       async customizeApplicationYml({ application: { llmModelName } }) {
         this.editFile(`src/main/resources/config/application-dev.yml`, { ignoreNonExisting: true }, content =>
-          content
-            .replace(
-              '\nspring:',
-              `\nspring:
+          content.replace(
+            '\nspring:',
+            `\nspring:
   ai:
     llama-cpp:
       model-home: '\${SPRING_AI_LLAMA_CPP_MODEL_HOME:models}'
@@ -146,93 +96,61 @@ export default class extends BaseApplicationGenerator {
       transformer:
         onnx.modelUri: '\${SPRING_AI_EMBEDDING_TRANSFORMER_ONNX_MODEL_URI:https://huggingface.co/intfloat/e5-small-v2/resolve/main/model.onnx}'
         tokenizer.uri: '\${SPRING_AI_EMBEDDING_TRANSFORMER_TOKENIZER_URI:https://huggingface.co/intfloat/e5-small-v2/raw/main/tokenizer.json}'`,
-            )
-            .replace(
-              '\n# application:',
-              `\n# application:
-openapi.my-llm-app.base-path: /api/llm/v1
-            `,
-            ),
+          ),
         );
       },
-      async customizeSecurityConfig({ application: { reactive } }) {
-        this.editFile(`src/main/java/com/mycompany/myapp/config/SecurityConfiguration.java`, { ignoreNonExisting: true }, content =>
-          reactive
-            ? content.replace('"/app/**",', `"/chat-ui/**", "/app/**",`).replace(
-                '.pathMatchers("/api/**").authenticated()',
-                `.pathMatchers("/api/llm/**").permitAll()
-                      .pathMatchers("/api/**").authenticated()`,
-              )
-            : content.replace(
-                '.requestMatchers(mvc.pattern("/api/**")).authenticated()',
-                `.requestMatchers(mvc.pattern("/api/llm/**")).permitAll()
-                    .requestMatchers(mvc.pattern("/chat-ui/**")).permitAll()
-                    .requestMatchers(mvc.pattern("/api/**")).authenticated()`,
-              ),
-        );
-      },
-      async cusomizeMaven({ source, application: { llmPomAdditions, buildToolMaven, reactive, llmModelName, llmModelUrl } }) {
+      async customizeBuildTool({ source, application: { buildToolMaven, javaDependencies, llmModelName, llmModelUrl } }) {
         if (buildToolMaven) {
-          source.addMavenProperty?.(Object.entries(llmPomAdditions.properties).map(([property, value]) => ({ property, value })));
+          source.addMavenProperty?.({ property: 'java-llamacpp.version', value: javaDependencies['java-llamacpp'] });
+          source.addMavenProperty?.({ property: 'llm.model.home', value: '${project.basedir}/models' });
           source.addMavenProperty?.({ property: 'llm.model.name', value: llmModelName });
           source.addMavenProperty?.({ property: 'llm.model.url', value: llmModelUrl });
-          source.addMavenRepository?.(llmPomAdditions.repositories);
-          source.addMavenDependency?.(llmPomAdditions.dependencies);
-          source.addMavenPlugin?.({ additionalContent: llmPomAdditions.buildPlugin });
-          this.editFile(`pom.xml`, content => {
-            if (content.includes('<artifactId>openapi-generator-maven-plugin</artifactId>')) {
-              content = content.replace(
-                '<version>${openapi-generator-maven-plugin.version}</version>',
-                `<version>\${openapi-generator-maven-plugin.version}</version>
-                <configuration>
-                    <typeMappings>integer=Long,int=Long</typeMappings>
-                </configuration>`,
-              );
-              content = content.replace(
-                '</useSpringBoot3>',
-                `</useSpringBoot3>                
-                <interfaceOnly>true</interfaceOnly>
-                <openApiNullable>false</openApiNullable>
-                ${!reactive ? '<reactive>true</reactive>' : ''}`, // for chat stream
-              );
-            }
-            return content;
+          source.addMavenDependency?.({
+            groupId: 'de.kherud',
+            artifactId: 'llama',
+            version: '${java-llamacpp.version}',
+          });
+          source.addMavenPlugin?.({
+            additionalContent: `
+            <groupId>com.github.eirslett</groupId>
+            <artifactId>frontend-maven-plugin</artifactId>
+            <configuration>
+                <skip>false</skip>
+            </configuration>                  
+            <executions>
+                <execution>
+                    <id>Download the integration test model if it doesn't exist</id>
+                    <phase>generate-resources</phase>
+                    <goals>
+                        <goal>npm</goal>
+                    </goals>
+                    <configuration>
+                        <arguments>run llm:download-model -- \${llm.model.home} \${llm.model.name} \${llm.model.url}</arguments>
+                    </configuration>
+                </execution>
+            </executions>
+          `,
           });
         }
       },
     });
   }
-
-  get [BaseApplicationGenerator.INSTALL]() {
-    return this.asInstallTaskGroup({
-      async installTemplateTask() {},
-    });
-  }
-
-  get [BaseApplicationGenerator.END]() {
-    return this.asEndTaskGroup({
-      async endTemplateTask() {},
-    });
-  }
 }
 
-const retrieveModels = () => {
-  return [
-    {
-      value: 'llama2',
-      name: 'Llama 2 (7B, 3.8GB)',
-      url: 'https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF/resolve/main/llama-2-7b-chat.Q4_0.gguf',
-    },
-    {
-      value: 'mistral',
-      name: 'Mistral (7B, 4.1GB)',
-      url: 'https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.2-GGUF/resolve/main/mistral-7b-instruct-v0.2.Q4_0.gguf',
-    },
-    { value: 'phi', name: 'Phi-2 (2.7B, 1.7GB)', url: 'https://huggingface.co/TheBloke/phi-2-GGUF/resolve/main/phi-2.Q4_0.gguf' },
-    {
-      value: 'codellama',
-      name: 'Code Llama (7B, 3.8GB)',
-      url: 'https://huggingface.co/TheBloke/CodeLlama-7B-Instruct-GGUF/resolve/main/codellama-7b-instruct.Q4_0.gguf',
-    },
-  ];
-};
+const retrieveModels = [
+  {
+    value: 'mistral',
+    name: 'Mistral (7B, 4.1GB)',
+    url: 'https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.2-GGUF/resolve/main/mistral-7b-instruct-v0.2.Q4_0.gguf',
+  },
+  {
+    value: 'llama2',
+    name: 'Llama 2 (7B, 3.8GB)',
+    url: 'https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF/resolve/main/llama-2-7b-chat.Q4_0.gguf',
+  },
+  {
+    value: 'codellama',
+    name: 'Code Llama (7B, 3.8GB)',
+    url: 'https://huggingface.co/TheBloke/CodeLlama-7B-Instruct-GGUF/resolve/main/codellama-7b-instruct.Q4_0.gguf',
+  },
+];
--- generators/spring-ai-ollama/generator.mjs
+++ generators/spring-ai-ollama/generator.mjs
@@ -1,8 +1,10 @@
 import BaseApplicationGenerator from 'generator-jhipster/generators/base-application';
-import { javaMainPackageTemplatesBlock, moveToJavaPackageSrcDir } from 'generator-jhipster/generators/java/support';
 import command from './command.mjs';
-import { getPomElements } from './utils-maven.mjs';
 export default class extends BaseApplicationGenerator {
+  async beforeQueue() {
+    await this.dependsOnJHipster('jhipster-llm:spring-ai');
+  }
+
   get [BaseApplicationGenerator.INITIALIZING]() {
     return this.asInitializingTaskGroup({
       async initializingTemplateTask() {
@@ -20,18 +22,12 @@ export default class extends BaseApplicationGenerator {
             type: 'list',
             name: 'llmModelName',
             message: 'Would you like to use a LLM model (https://ollama.com/library)?',
-            choices: retrieveOllamaModels(),
+            choices: retrieveModels,
             default: 'mistral',
           },
           this.blueprintStorage,
         );
       },
-    });
-  }
-
-  get [BaseApplicationGenerator.CONFIGURING]() {
-    return this.asConfiguringTaskGroup({
-      async configuringTemplateTask() {},
     });
   }
 
@@ -44,15 +40,6 @@ export default class extends BaseApplicationGenerator {
     });
   }
 
-  get [BaseApplicationGenerator.PREPARING]() {
-    return this.asPreparingTaskGroup({
-      async defaultTask({ application }) {
-        const pomFile = this.readTemplate(this.templatePath('../resources/pom.xml'));
-        application.llmPomAdditions = getPomElements(pomFile);
-      },
-    });
-  }
-
   get [BaseApplicationGenerator.WRITING]() {
     return this.asWritingTaskGroup({
       async writingTemplateTask({ application }) {
@@ -60,33 +47,8 @@ export default class extends BaseApplicationGenerator {
           sections: {
             llm: [
               {
-                ...javaMainPackageTemplatesBlock(),
-                templates: [
-                  'config/ChatCompletionRequestMessageDeserializer.java',
-                  'config/JacksonConfig.java',
-                  'web/rest/chat/ModelsApiController.java',
-                ],
-              },
-              {
-                condition: generator => generator.clientFrameworkAny,
-                path: `src/main/java/_package_/`,
-                renameTo: moveToJavaPackageSrcDir,
-                templates: [
-                  data => `web/rest/chat/ChatApiController_${data.imperativeOrReactive}.java`,
-                  data => `web/rest/chat/ChatApi_${data.imperativeOrReactive}.java`,
-                ],
-              },
-              {
-                path: 'src/main/resources',
-                templates: ['swagger/api.yml'],
-              },
-              {
                 path: 'src/main/docker',
                 templates: ['ollama.yml'],
-              },
-              {
-                path: './',
-                templates: ['models/download-model.mjs', 'models/README.md', 'build-chat.mjs'],
               },
             ],
           },
@@ -94,12 +56,6 @@ export default class extends BaseApplicationGenerator {
         });
         await this.copyTemplateAsync('../resources/base/{**,**/.*}', this.destinationPath());
       },
-    });
-  }
-
-  get [BaseApplicationGenerator.WRITING_ENTITIES]() {
-    return this.asWritingEntitiesTaskGroup({
-      async writingEntitiesTemplateTask() {},
     });
   }
 
@@ -122,123 +78,70 @@ export default class extends BaseApplicationGenerator {
         this.packageJson.merge({
           scripts: {
             'llm:ollama': 'docker exec -i ollama ollama',
-            'webapp:build:dev': 'webpack --config webpack/webpack.dev.js --env stats=minimal && npm run webapp:build:dev:chat --',
-            'webapp:build:dev:chat': 'node build-chat.mjs',
           },
         });
       },
-      async customizeMainApplication({ application: { mainJavaPackageDir, mainClass } }) {
-        this.editFile(`${mainJavaPackageDir}${mainClass}.java`, { ignoreNonExisting: true }, contents =>
-          contents
-            .replace(
-              "Application '{}' is running! Access URLs:",
-              `Application '{}' is running! Access URLs:
-                \\tChat: \\t\\t{}://localhost:{}{}chat-ui/index.html`,
-            )
-            .replace(
-              'applicationName,',
-              `applicationName,
-            protocol,
-            serverPort,
-            contextPath,`,
-            ),
-        );
-      },
       async customizeApplicationYml({ application: { llmModelName } }) {
         this.editFile(`src/main/resources/config/application-dev.yml`, { ignoreNonExisting: true }, content =>
-          content
-            .replace(
-              '\nspring:',
-              `\nspring:
+          content.replace(
+            '\nspring:',
+            `\nspring:
   ai:
     ollama:
       chat.options.model: ${llmModelName}
 `,
-            )
-            .replace(
-              '\n# application:',
-              `\n# application:
-openapi.my-llm-app.base-path: /api/llm/v1
-            `,
-            ),
+          ),
         );
       },
-      async customizeSecurityConfig({ application: { reactive } }) {
-        this.editFile(`src/main/java/com/mycompany/myapp/config/SecurityConfiguration.java`, { ignoreNonExisting: true }, content =>
-          reactive
-            ? content.replace('"/app/**",', `"/chat-ui/**", "/app/**",`).replace(
-                '.pathMatchers("/api/**").authenticated()',
-                `.pathMatchers("/api/llm/**").permitAll()
-                      .pathMatchers("/api/**").authenticated()`,
-              )
-            : content.replace(
-                '.requestMatchers(mvc.pattern("/api/**")).authenticated()',
-                `.requestMatchers(mvc.pattern("/api/llm/**")).permitAll()
-                    .requestMatchers(mvc.pattern("/chat-ui/**")).permitAll()
-                    .requestMatchers(mvc.pattern("/api/**")).authenticated()`,
-              ),
-        );
-      },
-      async cusomizeMaven({ source, application: { llmPomAdditions, buildToolMaven, reactive, llmModelName } }) {
+      async customizeBuildTool({ source, application: { buildToolMaven, llmModelName } }) {
         if (buildToolMaven) {
-          source.addMavenProperty?.(Object.entries(llmPomAdditions.properties).map(([property, value]) => ({ property, value })));
           source.addMavenProperty?.({ property: 'llm.model.name', value: llmModelName });
-          source.addMavenRepository?.(llmPomAdditions.repositories);
-          source.addMavenDependency?.(llmPomAdditions.dependencies);
-          source.addMavenPlugin?.({ additionalContent: llmPomAdditions.buildPlugin });
-          this.editFile(`pom.xml`, content => {
-            if (content.includes('<artifactId>openapi-generator-maven-plugin</artifactId>')) {
-              content = content.replace(
-                '<version>${openapi-generator-maven-plugin.version}</version>',
-                `<version>\${openapi-generator-maven-plugin.version}</version>
-                <configuration>
-                    <typeMappings>integer=Long,int=Long</typeMappings>
-                </configuration>`,
-              );
-              content = content.replace(
-                '</useSpringBoot3>',
-                `</useSpringBoot3>                
-                <interfaceOnly>true</interfaceOnly>
-                <openApiNullable>false</openApiNullable>
-                ${!reactive ? '<reactive>true</reactive>' : ''}`, // for chat stream
-              );
-            }
-            return content;
+          source.addMavenDependency?.({
+            groupId: 'org.springframework.ai',
+            artifactId: 'spring-ai-ollama-spring-boot-starter',
+            version: '${spring-ai.version}',
+          });
+          source.addMavenPlugin?.({
+            additionalContent: `
+            <groupId>com.github.eirslett</groupId>
+            <artifactId>frontend-maven-plugin</artifactId>
+            <configuration>
+                <skip>false</skip>
+            </configuration>                
+            <executions>
+                <execution>
+                    <id>Download the integration test model if it doesn't exist</id>
+                    <phase>generate-resources</phase>
+                    <goals>
+                        <goal>npm</goal>
+                    </goals>
+                    <configuration>
+                        <arguments>run llm:ollama pull \${llm.model.name}</arguments>
+                    </configuration>
+                </execution>
+            </executions>
+          `,
           });
         }
       },
     });
   }
-
-  get [BaseApplicationGenerator.INSTALL]() {
-    return this.asInstallTaskGroup({
-      async installTemplateTask() {},
-    });
-  }
-
-  get [BaseApplicationGenerator.END]() {
-    return this.asEndTaskGroup({
-      async endTemplateTask() {},
-    });
-  }
 }
 
-const retrieveOllamaModels = () => {
-  return [
-    { value: 'llama2', name: 'Llama 2 (7B, 3.8GB)' },
-    { value: 'mistral', name: 'Mistral (7B, 4.1GB)' },
-    { value: 'dolphin-phi', name: 'Dolphin Phi (2.7B, 1.6GB)' },
-    { value: 'phi', name: 'Phi-2 (2.7B, 1.7GB)' },
-    { value: 'neural-chat', name: 'Neural Chat (7B, 4.1GB)' },
-    { value: 'starling-lm', name: 'Starling (7B, 4.1GB)' },
-    { value: 'codellama', name: 'Code Llama (7B, 3.8GB)' },
-    { value: 'llama2-uncensored', name: 'Llama 2 Uncensored (7B, 3.8GB)' },
-    { value: 'llama2:13b', name: 'Llama 2 13B (13B, 7.3GB)' },
-    { value: 'llama2:70b', name: 'Llama 2 70B (70B, 39GB)' },
-    { value: 'orca-mini', name: 'Orca Mini (3B, 1.9GB)' },
-    { value: 'vicuna', name: 'Vicuna (7B, 3.8GB)' },
-    { value: 'llava', name: 'LLaVA (7B, 4.5GB)' },
-    { value: 'gemma:2b', name: 'Gemma (2B, 1.4GB)' },
-    { value: 'gemma:7b', name: 'Gemma (7B, 4.8GB)' },
-  ];
-};
+const retrieveModels = [
+  { value: 'mistral', name: 'Mistral (7B, 4.1GB)' },
+  { value: 'llama2', name: 'Llama 2 (7B, 3.8GB)' },
+  { value: 'dolphin-phi', name: 'Dolphin Phi (2.7B, 1.6GB)' },
+  { value: 'phi', name: 'Phi-2 (2.7B, 1.7GB)' },
+  { value: 'neural-chat', name: 'Neural Chat (7B, 4.1GB)' },
+  { value: 'starling-lm', name: 'Starling (7B, 4.1GB)' },
+  { value: 'codellama', name: 'Code Llama (7B, 3.8GB)' },
+  { value: 'llama2-uncensored', name: 'Llama 2 Uncensored (7B, 3.8GB)' },
+  { value: 'llama2:13b', name: 'Llama 2 13B (13B, 7.3GB)' },
+  { value: 'llama2:70b', name: 'Llama 2 70B (70B, 39GB)' },
+  { value: 'orca-mini', name: 'Orca Mini (3B, 1.9GB)' },
+  { value: 'vicuna', name: 'Vicuna (7B, 3.8GB)' },
+  { value: 'llava', name: 'LLaVA (7B, 4.5GB)' },
+  { value: 'gemma:2b', name: 'Gemma (2B, 1.4GB)' },
+  { value: 'gemma:7b', name: 'Gemma (7B, 4.8GB)' },
+];
