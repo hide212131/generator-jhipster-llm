1eacb23c638f72ae33a4cd00090f23e2171b43f0
Hideyuki Kagami <hide212131@gmail.com>
2024-03-31 15:14:40 +0900

    add options

--- generators/app/command.mjs
+++ generators/app/command.mjs
@@ -2,7 +2,23 @@
  * @type {import('generator-jhipster').JHipsterCommandDefinition}
  */
 const command = {
-  options: {},
+  options: {
+    llmFramework: {
+      description: 'LLM framework',
+      type: String,
+      scope: 'blueprint',
+    },
+    llmLibrary: {
+      description: 'LLM library',
+      type: String,
+      scope: 'blueprint',
+    },
+    llmModelName: {
+      description: 'LLM model',
+      type: String,
+      scope: 'blueprint',
+    },
+  },
 };
 
 export default command;
